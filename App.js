38b851ff390bc48d065b1cf51d291a1aa8a8f75c
jback22 <safaatan95@gmail.com>
2019-10-31 02:34:30 +0300

    safeareaview added

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import React,{useState} from 'react';
-import { StyleSheet, Text, View ,Button } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button, SafeAreaView} from 'react-native';
 import HeaderComp from "./HeaderComp";
 
 
 export default function App() {
 
-  return (
-    <View style={styles.container}>
-      <HeaderComp/>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <SafeAreaView style={styles.safearea}>
+                <Text></Text>
+            </SafeAreaView>
+            <HeaderComp/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop:50,
-  },
+    container: {
+        flex: 1
+    },
+    safearea: {
+        backgroundColor:'#EF3D75'
+    }
 });
